c93a7ec77c2eb29f7b4d5d2426e077efad687e52
Aakash Jangid <aakashjangid18@gmail.com>
2026-09-05 09:51:41 +0530

    Update employee attendance records and calculations

--- ass2/q4.py
+++ ass2/q4.py
@@ -2,44 +2,39 @@ from datetime import datetime
 
 employees = [
     {
-        "employee_name": "Rajesh Deshpande",
+        "employee_name": "Kishor Nand",
         "attendance": {
-            "2024-08-15": ("09:00", "17:00"),
-            "2024-08-16": ("09:15", "17:10"),
-            "2024-08-17": ("09:00", "17:00")
+            "2026-09-01": ("09:00", "17:00"),
+            "2026-09-02": ("09:15", "17:10"),
+            "2026-09-03": ("09:00", "17:00")
         }
     },
     {
-        "employee_name": "Amit Sharma",
+        "employee_name": "Shaam Kuru",
         "attendance": {
-            "2024-08-15": ("09:00", "17:00"),
-            "2024-08-16": ("09:00", "17:00")
+            "2026-09-01": ("09:00", "17:00"),
+            "2026-09-02": ("09:00", "17:00")
         }
     }
 ]
 
-# 1. Calculate total hours worked
-def total_hours(employee):
-    total = 0
-
-    for date, times in employee["attendance"].items():
+#1. total hours worked 
+def total_hours(employee): 
+    total = 0 
+    for date, times in employee["attendance"].items(): 
         clock_in = datetime.strptime(times[0], "%H:%M")
         clock_out = datetime.strptime(times[1], "%H:%M")
-
         hours = (clock_out - clock_in).seconds / 3600
         total += hours
-
     return total
 
-
-# 2. Calculate absences
-def absences(employee, total_days):
+# 2. absence
+def absence(employee, total_days): 
     present_days = len(employee["attendance"])
-    return total_days - present_days
+    return total_days - present_days 
 
 
-# Display result
-for employee in employees:
-    print("Employee:", employee["employee_name"])
-    print("Total hours:", total_hours(employee))
-    print()
+
+for employee in employees: 
+    print("\nEmployee: ", employee["employee_name"])
+    print("Total hours of working: ", total_hours(employee)) 
